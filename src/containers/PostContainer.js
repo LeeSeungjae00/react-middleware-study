11894344228a5941b442646099b61511ca22d421
LeeSeungjae <seungjae2668@gsinstrument.com>
2021-03-29 16:54:46 +0900

    thunk route 연습

---
+++ src/containers/PostContainer.js
@@ -0,0 +1,31 @@
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { getPost, clearPost, goToHome } from '../modules/posts';
+import Post from '../components/Post';
+
+function PostContainer({ postId }) {
+  const { data, loading, error } = useSelector(state => state.posts.post);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getPost(postId));
+    return () => {
+      dispatch(clearPost());
+    };
+  }, [postId, dispatch]);
+
+  if (loading) return <div>로딩중...</div>;
+  if (error) return <div>에러 발생!</div>;
+  if (!data) return null;
+
+  return (
+    <>
+      <button onClick = {()=> {dispatch(goToHome())}}>홈으로...</button>
+      <Post post={data} />
+    </>
+  );
+  
+  
+}
+
+export default PostContainer;
--- src/index.js
+++ src/index.js
@@ -2,16 +2,42 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import * as serviceWorker from './serviceWorker';
+import { applyMiddleware, createStore } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer, {rootSaga} from './modules';
+// import myLogger from './middlewares/myLogger';
+import logger from 'redux-logger'
+import { composeWithDevTools } from 'redux-devtools-extension';
+import ReduxThunk from 'redux-thunk';
+import { Router } from 'react-router-dom';
+import createSagaMiddleware from 'redux-saga';
+import { createBrowserHistory } from 'history';
+
+const customHistory = createBrowserHistory();
+
+const sagaMiddleware = createSagaMiddleware();
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(
+    ReduxThunk.withExtraArgument({history : customHistory}),
+    sagaMiddleware,logger))
+); // 여러개의 미들웨어를 적용 할 수 있습니다.
+
+
+sagaMiddleware.run(rootSaga)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router history = {customHistory}>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </Router>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+// serviceWorker.unregister();
